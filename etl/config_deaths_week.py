c0d9f28562f49b49c051472cc60db4b17c24d183
Alberto Lezcano <alberto@ayax.estudio.net>
2021-02-22 10:43:51 +0100

    Add 'defunciones-acumuladas-cantabria' graph

--- etl/config_deaths_week.py
+++ etl/config_deaths_week.py
@@ -602,6 +602,22 @@ params = {
             'note': [''],
             'json': 'defunciones-cantabria-graph.json-stat'
         },
+        'defunciones_acumuladas_cantabria': {
+            'sheet': 'Defunciones Acumuladas Cantabria Graph',
+            'label': 'Defunciones acumuladas por semana. Cantabria',
+            'variables': [
+                'Cantabria 2019',
+                'Cantabria 2020',
+                'Cantabria 2021'],
+            'source': '',
+            'unit':{
+                'Cantabria 2019': {'decimals': 0, 'label': 'Defunciones'},
+                'Cantabria 2020': {'decimals': 0, 'label': 'Defunciones'},
+                'Cantabria 2021': {'decimals': 0, 'label': 'Defunciones'}
+            },
+            'note': [''],
+            'json': 'defunciones-acumuladas-cantabria-graph.json-stat'
+        },
         'defunciones_cantabria_2020': {
             'sheet': 'Defunciones 2020',
             'label': 'Defunciones por semana Cantabria',
