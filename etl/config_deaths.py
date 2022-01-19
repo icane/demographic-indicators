546b4906381462339369a222cc434b7d18060f29
Alejandro López Cagigas <lopez_ale@cantabria.es>
2022-01-19 13:53:21 +0100

    Update data

--- etl/config_deaths.py
+++ etl/config_deaths.py
@@ -94,12 +94,10 @@ params = {
             'json': 'mortalidad-mujeres.json-stat'
         },
         '001_008': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '001-008  I.Enfermedades infecciosas y parasitarias',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_001-008  I.Enfermedades infecciosas y parasitarias_Cantabria'],
-            'moving_avg': [
                 'TM_001-008 Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -110,12 +108,10 @@ params = {
             'json': '001-008.json-stat'
         },
         '009_041': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '009-041  II.Tumores',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_009-041  II.Tumores_Cantabria'],
-            'moving_avg': [
                 'TM_009-041 Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -126,12 +122,10 @@ params = {
             'json': '009-041.json-stat'
         },
         '042_043': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '042-043  III.Enfermedades de la sangre y de los órganos hematopoyéticos, y ciertos trastornos que afectan al mecanismo de la inmunidad',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_042-043  III.Enfermedades de la sangre y de los órganos hematopoyéticos, y ciertos trastornos que afectan al mecanismo de la inmunidad_Cantabria'],
-            'moving_avg': [
                 'TM_042-043  Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -142,12 +136,10 @@ params = {
             'json': '042-043.json-stat'
         },
         '044_045': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '044-045  IV.Enfermedades endocrinas, nutricionales y metabólicas',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_044-045  IV.Enfermedades endocrinas, nutricionales y metabólicas_Cantabria'],
-            'moving_avg': [
                 'TM_044-045 Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -158,12 +150,10 @@ params = {
             'json': '044-045.json-stat'
         },
         '046_049': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '046-049  V.Trastornos mentales y del comportamiento',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_046-049  V.Trastornos mentales y del comportamiento_Cantabria'],
-            'moving_avg': [
                 'TM_046-049 Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -174,12 +164,10 @@ params = {
             'json': '046-049.json-stat'
         },
         '050_052': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '050-052  VI-VIII.Enfermedades del sistema nervioso y de los órganos de los sentidos',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_050-052  VI-VIII.Enfermedades del sistema nervioso y de los órganos de los sentidos_Cantabria'],
-            'moving_avg': [
                 'TM_050-052  Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -190,12 +178,10 @@ params = {
             'json': '050-052.json-stat'
         },
         '053_061': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '053-061 IX.Enfermedades del sistema circulatorio',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_053-061 IX.Enfermedades del sistema circulatorio_Cantabria'],
-            'moving_avg': [
                 'TM_053-061 Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -206,12 +192,10 @@ params = {
             'json': '053-061.json-stat'
         },
         '062_067': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '062-067  X.Enfermedades del sistema respiratorio',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_062-067  X.Enfermedades del sistema respiratorio_Cantabria'],
-            'moving_avg': [
                 'TM_062-067  Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -222,12 +206,10 @@ params = {
             'json': '062-067.json-stat'
         },
         '068_072': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '068-072  XI.Enfermedades del sistema digestivo',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_068-072  XI.Enfermedades del sistema digestivo_Cantabria'],
-            'moving_avg': [
                 'TM_068-072  Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -238,12 +220,10 @@ params = {
             'json': '068-072.json-stat'
         },
         '073': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '073  XII.Enfermedades de la piel y del tejido subcutáneo',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_073  XII.Enfermedades de la piel y del tejido subcutáneo_Cantabria'],
-            'moving_avg': [
                 'TM_073  Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -254,12 +234,10 @@ params = {
             'json': '073.json-stat'
         },
         '074_076': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '074-076  XIII.Enfermedades del sistema osteomuscular y del tejido conjuntivo',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_074-076  XIII.Enfermedades del sistema osteomuscular y del tejido conjuntivo_Cantabria'],
-            'moving_avg': [
                 'TM_074-076  Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -270,12 +248,10 @@ params = {
             'json': '074-076.json-stat'
         },
         '077_080': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '077-080  XIV.Enfermedades del sistema genitourinario',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_077-080  XIV.Enfermedades del sistema genitourinario_Cantabria'],
-            'moving_avg': [
                 'TM_077-080 Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -286,12 +262,10 @@ params = {
             'json': '077-080.json-stat'
         },
         '081': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '081  XV.Embarazo, parto y puerperio',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_081  XV.Embarazo, parto y puerperio_Cantabria'],
-            'moving_avg': [
                 'TM_081  Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -302,12 +276,10 @@ params = {
             'json': '081.json-stat'
         },
         '082': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '082  XVI.Afecciones originadas en el periodo perinatal',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_082  XVI.Afecciones originadas en el periodo perinatal_Cantabria'],
-            'moving_avg': [
                 'TM_082 Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -318,12 +290,10 @@ params = {
             'json': '082.json-stat'
         },
         '083_085': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '083-085  XVII.Malformaciones congénitas, deformidades y anomalías cromosómicas',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_083-085  XVII.Malformaciones congénitas, deformidades y anomalías cromosómicas_Cantabria'],
-            'moving_avg': [
                 'TM_083-085 Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -334,12 +304,10 @@ params = {
             'json': '083-085.json-stat'
         },
         '086_089': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '086-089  XVIII.Síntomas, signos y hallazgos anormales clínicos y de laboratorio, no clasificados en otra parte',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_086-089  XVIII.Síntomas, signos y hallazgos anormales clínicos y de laboratorio, no clasificados en otra parte_Cantabria'],
-            'moving_avg': [
                 'TM_086-089 Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
@@ -350,12 +318,10 @@ params = {
             'json': '086-089.json-stat'
         },
         '090_102': {
-            'sheet': 'Causas de muerte meses',
+            'sheet': 'Causas_media_móvil_3meses_Canta',
             'label': '090-102  XX.Causas externas de mortalidad',
             'category': 'Defunciones por causa de muerte. Tasas de defunciones por 100.000 habitantes',
             'variables': [
-                'TM_090-102  XX.Causas externas de mortalidad_Cantabria'],
-            'moving_avg': [
                 'TM_090-102 Media móvil centrada (3 meses)'],
             'source': 'ICANE a partir de la explotación de microdatos de Defunciones por causa de muerte, INE',
             'unit':{
